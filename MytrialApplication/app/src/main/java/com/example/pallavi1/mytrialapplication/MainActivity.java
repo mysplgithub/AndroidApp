895134216ae02aeff25b9502e2df0715ac8d1969
pallavi <pallavi.labade148@gmail.com>
2017-02-04 00:52:11 +0530

    Android Hello world Programm

---
+++ MytrialApplication/app/src/main/java/com/example/pallavi1/mytrialapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pallavi1.mytrialapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
